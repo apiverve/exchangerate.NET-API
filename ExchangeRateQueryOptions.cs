a426ae256aa18fd505d3adf60fb9e0d04f9d5dc1
APIVerve Team <hello@apiverve.com>
2025-03-28 09:30:59 -0500

    Bug Fixes

---
+++ ExchangeRateQueryOptions.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class ExchangeRateQueryOptions {
+public string currency1 { get; set; }
+public string currency2 { get; set; }
+}
+
+}
